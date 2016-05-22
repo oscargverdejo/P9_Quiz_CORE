6aab7c8e1bbc1245b14261db58d6da1c8c9109be
Oscar Garcia Verdejo <oscargverdejo@hotmail.com>
2016-05-22 21:25:36 +0200

    Tema 17: Migraciones y Seeders

--- models/index.js
+++ models/index.js
@@ -24,26 +24,4 @@ var sequelize = new Sequelize(url,
 //Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
-// sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize
-.sync()
-.then(function() {
-	return Quiz
-		.count()
-		.then(function (c){
-			if (c === 0){
-				return Quiz
-					.bulkCreate([{ question: 'Capital de Italia', answer: 'Roma'},
-								 { question: 'Capital de Portugal', answer: 'Lisboa'}
-								 ])
-					.then(function(){
-						console.log('Base de datos inicializada con datos');
-					});
-			}
-		});
-}).catch(function(error) {
-	console.log("Error sincronizando las tablas de la BBDD", error);
-	process.exit(1);
-});
-
 exports.Quiz = Quiz;//exportar definicion de la tabla Quiz
